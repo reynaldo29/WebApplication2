7c9489e0f4dd316716182a3b0098f16353338fc5
acm19 <acm19@outlook.com>
2023-05-23 17:07:20 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    
+    public class productController : Controller
+    {
+        // GET: product
+        private NeptunoEntities db = new NeptunoEntities();
+        public ActionResult Index()
+        {
+            return View(db.productos.ToList());
+        }
+
+        // GET: clientes/Producto
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idProducto,nombreProducto,cantidadPorUnidad,precioUnidad,categoriaProducto")] productos productos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.productos.Add(productos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productos);
+        }
+    }
+
+
+}
